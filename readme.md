dcd2628b810763e42194bd96d22643b4bf62fd61
Paul G <pganssle@users.noreply.github.com>
2015-02-16 15:37:42 -0600

    Add sequential subset.

--- readme.md
+++ readme.md
@@ -13,16 +13,95 @@ the episode.
 This repository explores how to optimally sample the episode so as to get as close as possible to
 the true RMS in the smallest number of samples, using various different sampling techniques.
 
-The demo uses the following podcasts, of various mastering qualities:
+The demo uses the following podcasts, of various mastering qualities:<sup><a href="#ref1">1</a></sup>
 * [Serial, Season 1 Episode 7: *The Opposite of Prosecution*](http://serialpodcast.org/season-one/7/the-opposite-of-the-prosecution) (`serial-episode-07.mp3`, 32m 34s)
 * [Seattle Library Podcast - October 28, 2014: *Polio Then and Now: From Salk’s Game-Changing Vaccine to Today’s Resurgence*](http://www.spl.org/Audio/14_10_28_Rob_Lin.mp3) (`seattle-library-podcast-2014-10-28.mp3`, 1h 17m 02s)
 * [Spy Museum Podcast - Author Debriefing: *Good Hunting, An American Spymaster's Story*](http://www.spymuseum.org/multimedia/spycast/episode/author-debriefing-good-hunting-an-american-spymasters-story/) (`spy-museum-podcast-2014-11-07.mp3`, 1h 07m 42s)
 * [Federalist Society Events Audio - *Defining Regulatory Crimes*](http://www.fed-soc.org/multimedia/detail/defining-regulatory-crimes-event-audiovideo) (`federalist-society-events-2014-06-11`, 1h 19m 06s)
 
-Choosing random samples from these files totaling 2 seconds (~88,000 samples usually), and repeated
-100 times gives the following results:
+### Sequential subset
 
-    >>> python run-demo.py -r 100 -rd 2.0 
+To get an estimate of how many samples it takes to get close to the true value, I can calculate the
+true loudness of the file to start with, then try reading it out until I get close enough to the
+true value. Retrieving samples from the start, middle and end of each file, until I get to within
+0.2 dB of the true value gives me:
+    >>> python run-demo.py -ds -sh -uc -uc -ms 100000000
+    serial-episode-07.mp3
+    True dBFS: -19.037004
+    Start number of samples: 23846 (-19.236950 dB)
+    Middle number of samples: 1187 (-18.840281 dB)
+    End number of samples: 131014 (-19.236404 dB)
+
+    seattle-library-podcast-2014-10-28.mp3
+    True dBFS: -34.131281
+    Start number of samples: 10215 (-34.331054 dB)
+    Middle number of samples: 86860 (-33.931294 dB)
+    End number of samples: 248471 (-34.331107 dB)
+
+    spy-museum-podcast-2014-11-07.mp3
+    True dBFS: -21.969535
+    Start number of samples: 7350369 (-22.169532 dB)
+    Middle number of samples: 78544 (-22.167233 dB)
+    End number of samples: 3105373 (-22.169480 dB)
+
+    federalist-society-events-2014-06-11.mp3
+    True dBFS: -25.349533
+    Start number of samples: 26262143 (-25.549532 dB)
+    Middle number of samples: 96944106 (-25.549533 dB)
+    End number of samples: 78138 (-25.545350 dB)
+
+It seems like in many cases, if you're going to pick a place and start sampling, the middle is the
+best place to start - but the Federalist Society event podcast is a notable exception to this - with
+the middle being the absolute *worst* place to start. In any case, the worst case scenario here
+(the Federalist Society podcast, starting in the middle), still only takes 36 minutes of audio to
+get us the data we need - which isn't great, but it's still less than 50% of the total duration of
+the episode, so it's a start.
+
+### Random Subset
+
+Rather than choosing samples sequentially, if we choose them at random, it's very unlikely that
+you'll take *too many* samples from any given quiet or loud region, so you should be able to get
+close to the true value much more quickly. Repeating the above test using random subsampling (and
+because it's not the same results every time, repeating *that* test 150 times), you get:
+
+    >>> python run-demo.py -dr -rr 150 -uc --min-samples 100
+    serial-episode-07.mp3
+    True dBFS: -19.037004
+    Final dBFS: -18.857259
+    Mean number of samples: 534.286667 (24.230688 ms)
+    Min number of samples: 101.000000 (4.580499 ms)
+    Max number of samples: 7666.000000 (347.664399 ms)
+
+    seattle-library-podcast-2014-10-28.mp3
+    True dBFS: -34.131281
+    Final dBFS: -33.936553
+    Mean number of samples: 1517.386667 (34.407861 ms)
+    Min number of samples: 101.000000 (2.290249 ms)
+    Max number of samples: 66808.000000 (1514.920635 ms)
+
+    spy-museum-podcast-2014-11-07.mp3
+    True dBFS: -21.969535
+    Final dBFS: -21.773998
+    Mean number of samples: 1000.406667 (22.684958 ms)
+    Min number of samples: 101.000000 (2.290249 ms)
+    Max number of samples: 31924.000000 (723.900227 ms)
+
+    federalist-society-events-2014-06-11.mp3
+    True dBFS: -25.349533
+    Final dBFS: -25.152401
+    Mean number of samples: 2160.993333 (49.002116 ms)
+    Min number of samples: 101.000000 (2.290249 ms)
+    Max number of samples: 38802.000000 (879.863946 ms)
+
+So it looks like the absolute worst case performance here is about 1.5 seconds - considerably less 
+than the 36 minutes from the sequential test! Of course, we can't assume that we know the true value 
+of the loudness, since that's what we're trying to measure, so we have a question of when to stop 
+taking random samples. For the moment, we can sidestep that problem by just choosing a fixed 
+duration worth of random samples and seeing how close that gets us to the true value. Sampling 2 
+seconds (44,000 or 88,000 samples) worth of audio from each of these files 150 times gives the 
+following results:
+
+    >>> python run-demo.py -dr -rr 150 -d 2.0 
     serial-episode-07.mp3
     True dBFS: -19.037004
     Mean dBFS: -19.050726 (Error: 0.013722)
@@ -57,3 +136,8 @@ Using [mp3Gain](https://en.wikipedia.org/wiki/MP3Gain) on these same files and s
 see, the most naive method of random sampling, using only 0.04-0.1% of the total length of the file
 to ascertain the overall volume level with enough accuracy to significantly improve the quality of
 one's listening experience!
+
+#### Footnotes
+1. <a name="ref1" />I picked one from the Top 10 in iTunes, since that's probably mastered well and 
+   normalized optimally, then I tried to fill out the rest with live or "event" type podcasts, which 
+   tend to be more unevenly mastered in my experience.
--- run-demo.py
+++ run-demo.py
@@ -25,10 +25,17 @@ from pydub.pyaudioop import getsample
 from math import log, sqrt
 import numpy as np
 
+import matplotlib.pyplot as plt
 import subset_generators as sg
 
 # Parse arguments
 parser = argparse.ArgumentParser()
+
+parser.add_argument('-dr', '--do-random', action='store_true',
+                    help='Run the random subset test')
+
+parser.add_argument('-ds', '--do-sequential', action='store_true',
+                    help='Run the sequential subset test')
 
 parser.add_argument('-uc', '--until-close', action='store_true',
                     help='If this flag is used, the random subset generator will pull samples '+
@@ -37,14 +44,28 @@ parser.add_argument('-uc', '--until-close', action='store_true',
 parser.add_argument('--error-factor', default=0.2, type=float,
                     help='The target error factor in dB')
 
-parser.add_argument('-r', '--repeat-random', default=50, type=int,
+parser.add_argument('-rr', '--repeat-random', default=50, type=int,
                     help='The number of times to repeat the random subset generator test.')
 
-parser.add_argument('-rd', '--rand-dur', default=2.0, type=float,
-                    help='The total amount of the file to randomly subsample, seconds.')
+parser.add_argument('-d', '--dur', default=2.0, type=float,
+                    help='The total amount of the file to subsample, in seconds.')
 
 parser.add_argument('-sh', '--skip-hash', action='store_true', 
                     help='If present, skip the hash check on the mp3s')
+
+parser.add_argument('-sf', '--save-figs', action='store_true',
+                    help='If used, figures will be saved.')
+
+parser.add_argument('-shf', '--show-figs', action='store_true',
+                    help='If used, figures will be displayed.')
+
+parser.add_argument('-pd', '--plot-decimation', default=1,
+                    help='For large arrays, it may be preferable to decimate the arrays to save memory.')
+
+parser.add_argument('-mis', '--min-samples', default=100, type=int,
+                    help='Minimum number of samples to collect in until-close mode.')
+parser.add_argument('-ms', '--max-samples', default=10000000, type=int,
+                    help='Maximum number of samples before you give up.')
 
 args = parser.parse_args()
 
@@ -52,6 +73,8 @@ args = parser.parse_args()
 media_loc = 'media'
 mp3_loc = os.path.join(media_loc, 'mp3')
 wav_loc = os.path.join(media_loc, 'wav')
+
+sequential_fig_outputs = 'outputs/sequential'
 
 # Make sure that we have all the files for the demo.
 with open('demo-file-info.json', 'r') as jsonfile:
@@ -79,6 +102,7 @@ for demo_file in f_list:
             sha256_hash.update(mdfile.read())
 
             if sha256_hash.hexdigest() != demo_file['checksum']:
+                print(sha256_hash.hexdigest())
                 warnings.warn("File " + demo_file['name'] + '.' + demo_file['type'] + 
                               ' has non-matching checksum, skipping.', RuntimeWarning)
                 continue
@@ -87,12 +111,21 @@ for demo_file in f_list:
     f_types.append(demo_file['type'])
 
 
+def update_running_sum(samp, running_sum_sq, n_samps):
+    """
+    Update the running sum and return the RMS.
+    """
+    running_sum_sq += samp**2
+    rms = sqrt(running_sum_sq / n_samps)
+
+    return (running_sum_sq, rms)
+
 calc_dbfs = lambda rms, segment: (20 * log(rms / segment.max_possible_amplitude, 10)) if rms > 0 else -float('infinity')
 files_processed = []
 
 # Start calculating RMS values.
 for floc, ftype in zip(f_locations, f_types):
-    cname = os.path.split(floc)[1]
+    cname = os.path.split(floc)[1][:-4]
 
     seg = None
     gc.collect()        # These files can be large in memory - clean up explicitly.
@@ -102,84 +135,194 @@ for floc, ftype in zip(f_locations, f_types):
 
     true_dbfs = seg.dBFS
     
-    print('{}\nTrue dBFS: {:f}'.format(cname, true_dbfs))
+    print('{}\nTrue dBFS: {:f}'.format(cname+'.mp3', true_dbfs))
 
+    #
     # Random subset
-    rand_dbfses = []
-    rand_dbfses_final = []
-    n_samp_vals = []
-    
-    for ii in xrange(args.repeat_random):
+    #
+    if args.do_random:
+        rand_dbfses = []
+        rand_dbfses_final = []
+        n_samp_vals = []
+        samprange = total_samps if args.until_close else np.ceil(args.dur * seg.frame_rate)
+        if samprange > args.max_samples:
+            samprange = args.max_samples
+        
+        for ii in xrange(args.repeat_random):
+            rsub = sg.RandomSubsetGenerator(total_samps, n=samprange)
+
+            samps = []
+            rand_dbfs = []
+            running_sum_sq = 0
+            n_samps = 0
+
+            for ind in rsub:
+                n_samps += 1
+                samp = getsample(seg._data, seg.sample_width, ind)
+
+                samps.append(samp)
+
+                # Calculate RMS
+                running_sum_sq, rms = update_running_sum(samp, running_sum_sq, n_samps)
+                dbfs = calc_dbfs(rms, seg)
+
+                rand_dbfses.append(dbfs)
+
+                if args.until_close and n_samps > args.min_samples and \
+                   abs(dbfs - true_dbfs) < args.error_factor:
+                    break
+
+            n_samp_vals.append(n_samps)
+            rand_dbfses.append(rand_dbfs)
+            rand_dbfses_final.append(dbfs)
+
         if args.until_close:
-            # Select a random subset of the segment and start calculating the RMS.
-            # Stop when it reaches the designated error factor
-            rsub = sg.RandomSubsetGenerator(total_samps)
+            mean_n_samps = np.mean(n_samp_vals)
+            samps2ms = 1000 / seg.frame_rate
+            print(('Final dBFS: {:f}\n'+ 
+                  'Mean number of samples: {mean_n:f} ({mean_ns:f} ms)\n' + 
+                  'Min number of samples: {min_n:f} ({min_ns:f} ms)\n' +
+                  'Max number of samples: {max_n:f} ({max_ns:f} ms)\n')
+                  .format(dbfs,
+                          mean_n=mean_n_samps,
+                          mean_ns=mean_n_samps * samps2ms,
+                          min_n=np.min(n_samp_vals),
+                          min_ns=np.min(n_samp_vals) * samps2ms,
+                          max_n=np.max(n_samp_vals),
+                          max_ns=np.max(n_samp_vals) * samps2ms))
         else:
-            rsub = sg.RandomSubsetGenerator(total_samps, n=np.ceil(args.rand_dur * seg.frame_rate))
+            mean_db = np.mean(rand_dbfses_final)
+            max_db = np.max(rand_dbfses_final)
+            min_db = np.min(rand_dbfses_final)
 
-        samps = []
-        rand_dbfs = []
-        running_sum_sq = 0
-        n_samps = 0
+            print(('Mean dBFS: {mean:f} (Error: {meane:f})\n' +
+                   'Min dBFS: {min:f} (Error: {mine:f})\n' + 
+                   'Max dBFS: {max:f} (Error: {maxe:f})\n' + 
+                   'Standard Deviation:  {std:f}\n').format(mean=mean_db,
+                                                            meane=abs(true_dbfs-mean_db),
+                                                            min=min_db,
+                                                            mine=abs(true_dbfs-min_db),
+                                                            max=max_db,
+                                                            maxe=abs(true_dbfs-max_db),
+                                                            std=np.std(rand_dbfses_final)))
 
-        for ind in rsub:
-            n_samps += 1
-            samp = getsample(seg._data, seg.sample_width, ind)
+    #
+    # Start from beginning, end and middle
+    #
+    if args.do_sequential:
+        running_sum_sq_s = 0
+        running_sum_sq_m = 0
+        running_sum_sq_e = 0
 
-            samps.append(samp)
+        middle_ind = total_samps//2
 
-            # Calculate RMS
-            running_sum_sq += samp ** 2
-            rms = sqrt(running_sum_sq / n_samps)
-            dbfs = calc_dbfs(rms, seg)
+        n_samps_s = 0
+        n_samps_m = 0
+        n_samps_e = 0
 
-            rand_dbfses.append(dbfs)
+        s_done = False
+        m_done = False
+        e_done = False
 
-            if args.until_close and abs(dbfs - true_dbfs) < args.error_factor:
+        keep_arrays = args.show_figs or args.save_figs
+        dbfses_s = []
+        dbfses_m = []
+        dbfses_e = []
+
+        samprange = total_samps if args.until_close else int(np.ceil(args.dur * seg.frame_rate))
+
+        if samprange > args.max_samples:
+            samprange = args.max_samples
+
+        for ii in xrange(samprange):
+            if not s_done:
+                samp = getsample(seg._data, seg.sample_width, ii)
+                n_samps_s += 1
+
+                running_sum_sq_s, rms_s = update_running_sum(samp, running_sum_sq_s, n_samps_s)
+                dbfs_s = calc_dbfs(rms_s, seg)
+
+                if keep_arrays and (ii % self.plot_decimation == 0):
+                    dbfses_s.append(dbfs_s)
+
+                if args.until_close and abs(dbfs_s - true_dbfs) < args.error_factor:
+                    s_done = True
+            
+            if not m_done:
+                samp = getsample(seg._data, seg.sample_width, (ii+middle_ind)%total_samps)
+                n_samps_m += 1
+
+                running_sum_sq_m, rms_m = update_running_sum(samp, running_sum_sq_m, n_samps_m)
+                dbfs_m = calc_dbfs(rms_m, seg)
+
+                if keep_arrays and (ii % self.plot_decimation == 0):
+                    dbfses_m.append(dbfs_m)
+
+                if args.until_close and abs(dbfs_m - true_dbfs) < args.error_factor:
+                    m_done = True
+
+            if not e_done:
+                samp = getsample(seg._data, seg.sample_width, total_samps-(ii+1))
+                n_samps_e += 1
+
+                running_sum_sq_e, rms_e = update_running_sum(samp, running_sum_sq_e, n_samps_e)
+                dbfs_e = calc_dbfs(rms_e, seg)
+
+                if keep_arrays and (ii % self.plot_decimation == 0):
+                    dbfses_e.append(dbfs_e)
+
+                if args.until_close and abs(dbfs_e - true_dbfs) < args.error_factor:
+                    e_done = True
+
+            if s_done and m_done and e_done:
                 break
 
-        n_samp_vals.append(n_samps)
-        rand_dbfses.append(rand_dbfs)
-        rand_dbfses_final.append(dbfs)
+        print(('Start number of samples: {start_ns:d} ({start_db:f} dB)\n' +
+              'Middle number of samples: {middle_ns:d} ({middle_db:f} dB)\n' +
+              'End number of samples: {end_ns:d} ({end_db:f} dB)\n')
+              .format(start_ns=n_samps_s,
+                      start_db=dbfs_s,
+                      middle_ns=n_samps_m,
+                      middle_db=dbfs_m,
+                      end_ns=n_samps_e,
+                      end_db=dbfs_e))
 
-    if args.until_close:
-        mean_n_samps = np.mean(n_samp_vals)
-        samps2ms = 1000 / seg.frame_rate
-        print(('Final dBFS: {:f}\n'+ 
-              'Mean number of samples: {mean_n:f} ({mean_ns:f} ms)\n' + 
-              'Min number of samples: {min_n:f} ({min_ns:f} ms)\n' +
-              'Max number of samples: {max_n:f} ({max_ns:f} ms)\n')
-              .format(dbfs,
-                      mean_n=mean_n_samps,
-                      mean_ns=mean_n_samps * samps2ms,
-                      min_n=np.min(n_samp_vals),
-                      min_ns=np.min(n_samp_vals) * samps2ms,
-                      max_n=np.max(n_samp_vals),
-                      max_ns=np.max(n_samp_vals) * samps2ms))
-    else:
-        mean_db = np.mean(rand_dbfses_final)
-        max_db = np.max(rand_dbfses_final)
-        min_db = np.min(rand_dbfses_final)
+        if args.show_figs or args.save_figs:
+            fig = plt.figure(figsize=(8, 3))
+            
+            phs, = plt.plot(range(n_samps_s), dbfses_s, '-b')
+            phm, = plt.plot(range(n_samps_m), dbfses_m, '-r')
+            phe, = plt.plot(range(n_samps_e), dbfses_e, '-g')
 
-        print(('Mean dBFS: {mean:f} (Error: {meane:f})\n' +
-               'Min dBFS: {min:f} (Error: {mine:f})\n' + 
-               'Max dBFS: {max:f} (Error: {maxe:f})\n' + 
-               'Standard Deviation:  {std:f}\n').format(mean=mean_db,
-                                                        meane=abs(true_dbfs-mean_db),
-                                                        min=min_db,
-                                                        mine=abs(true_dbfs-min_db),
-                                                        max=max_db,
-                                                        maxe=abs(true_dbfs-max_db),
-                                                        std=np.std(rand_dbfses_final)))
+            pht, = plt.plot([0, np.max([n_samps_s, n_samps_m, n_samps_e])], [true_dbfs, true_dbfs],
+                            '--k')
 
+            plt.xlim(0, np.max([n_samps_s, n_samps_m, n_samps_e]))
 
+            plt.legend([pht, phs, phm, phe], ['True dbFS', 'Start', 'Middle', 'End'], loc='best', fontsize=9)
 
-    cfile = {
-              'name': cname,
-              'true_dbfs': true_dbfs,
-              'rand_dbfses': rand_dbfses,
-              'rand_samps': samps,
-              'n_samps': n_samp_vals
-            }
+            fig.canvas.set_window_title('Sequential Subset: '+cname)
 
-    files_processed.append(cfile)
+            plt.title('Sequential Subset: ' + cname, fontsize=10)
+
+            plt.xlabel('Samples', fontsize=9)
+            plt.ylabel('Loudness (dB)', fontsize=9)
+
+            plt.tick_params(axis='both', labelsize=7)
+
+            plt.tight_layout()
+
+            if args.save_figs:
+                if not os.path.exists(sequential_fig_outputs):
+                    os.makedirs(sequential_fig_outputs)
+
+                if args.until_close:
+                    cfname = cname + ' (Until Close).png'
+                else:
+                    cfname = cname + ' ({:08d samples}).png'.format(n_samps_s)
+
+                plt.savefig(os.path.join(sequential_fig_outputs, cfname), format='png',
+                            transparent=True, dpi=300)
+
+            if not args.show_figs:
+                fig.close()
